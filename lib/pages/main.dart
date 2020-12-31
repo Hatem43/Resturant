f90346b6797198f03a9e1d917abd91e8e78949dd
Hatem43 <63131768+Hatem43@users.noreply.github.com>
2020-12-31 04:45:37 +0200

    Update main.dart

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: CinemasPage(),
+      home: CinemaPage(),
       routes: <String, WidgetBuilder>{
         "Login": (BuildContext context) => LoginScreen(),
         "Signup": (BuildContext context) => SignupScreen(),
